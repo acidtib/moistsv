6b2eaeca45e94126efec4de1aed62a12e880e08d
arubinofaux <arubinofaux@gmail.com>
2019-06-02 01:31:50 -0400

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,70 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="page-container">
+    <md-app md-waterfall md-mode="overlap">
+      <md-app-toolbar class="bg-white md-large">
+        <div class="md-toolbar-row">
+          <div class="md-toolbar-section-start">
+            <div class="header-logo">
+              <router-link to="/"><img src="./assets/logo.png"></router-link>
+            </div>
+          </div>
+
+          <div class="md-toolbar-section-end search-txs">
+            <md-field md-inline>
+              <label>Search Transaction Hash</label>
+              <md-input v-model="txhash"></md-input>
+            </md-field>
+          </div>
+        </div>
+      </md-app-toolbar>
+
+      <md-app-content>
+        <router-view/>
+      </md-app-content>
+    </md-app>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<style lang="scss" scoped>
+  .header-logo {
+    margin: 31px 0px 0px 5px;
+    
+    img {
+      width: 200px;
     }
   }
-}
+
+  .bg-white {
+    background-color: #ffffff;
+  }
+
+  .search-txs {
+    .md-field.md-theme-default.md-focused .md-input {
+      color: #000000;
+    }
+    .md-field.md-theme-default.md-focused .md-input,
+    .md-field.md-theme-default.md-has-value .md-input {
+      -webkit-text-fill-color: #000000;
+    }
+    .md-field.md-theme-default label {
+      color: #000000;
+    }
+    .md-field.md-theme-default:before {
+      background-color: #000000;
+    }
+    .md-field:before {
+      opacity: 1;
+      transform: none;
+    }
+  }
 </style>
+
+<script>
+export default {
+  name: 'Overlap',
+  data: () => ({
+    menuVisible: false,
+    txhash: null
+  })
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+    <md-field class="pk">
+      <label>Private Key:</label>
+      <md-input v-model="pk" type="password"></md-input>
+      <span class="md-helper-text">All the bitcoin magic takes place on the client side, so your private key is safe.</span>
+    </md-field>
+
+    <md-field class="payload">
+      <label>Data:</label>
+      <md-textarea v-model="payload"></md-textarea>
+      <span class="md-helper-text">Data that will be stored on the BitcoinSV blockchain.</span>
+    </md-field>
+
+    <div>
+      <md-button class="md-raised md-primary moist-button" v-on:click="moistit">Moist It Up!</md-button>
+    </div>
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+<style lang="scss" scoped>
+  .home {
+    .payload {
+      margin-bottom: 42px;
+    }
+    .pk {
+      margin-bottom: 47px;
+    }
+    .moist-button {
+      margin: 0px;
+      width: 100%;
+    }
   }
-}
+</style>
+
+<script>
+  export default {
+    name: 'home',
+    data: () => ({
+      pk: null,
+      payload: null
+    }),
+    methods: {
+      moistit: function(event) {
+        console.log(this.pk)
+        console.log(this.payload)
+      }
+    }
+  }
 </script>
