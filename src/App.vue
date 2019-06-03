dfa534ca3a36e1653c546fd6bebf73245aa3ba03
arubinofaux <arubinofaux@gmail.com>
2019-06-03 02:59:19 -0400

    add footer

--- src/App.vue
+++ src/App.vue
@@ -22,10 +22,27 @@
         <router-view/>
       </md-app-content>
     </md-app>
+
+    <footer class="main-footer">
+      <div class="main-footer-container">
+        <div class="main-footer-section">
+          <a href="https://github.com/arubinofaux/moistsv" target="_blank">Click here to review the code in Github</a>
+        </div>
+        <div class="main-footer-section">
+          <a href="https://twitter.com/acidtib_" target="_blank">Made with <span class="red">❤</span> by acidtib_</a>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
 <style lang="scss" scoped>
+  .page-container {
+    min-height: 100%;
+    display: flex;
+    flex-direction: column;
+    font-family: Roboto Mono,monospace;
+  }
   .header-logo {
     margin: 31px 0px 0px 5px;
     
@@ -56,6 +73,36 @@
       opacity: 1;
       transform: none;
     }
+  }
+
+  .main-footer {
+    height: 96px;
+    padding: 0 16px;
+    position: relative;
+    z-index: 4;
+    background-color: #ffffff;
+    
+    a {
+      color: #000000;
+    }
+
+    .main-footer-container {
+      max-width: 100%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      transition: .3s cubic-bezier(.4,0,.2,1);
+      transition-property: max-width;
+
+      .main-footer-section {
+        position: relative;
+      }
+    }
+  }
+
+  .red {
+    color: red;
   }
 </style>
 
